786a924684de3b54833edc6f78fe896cae0a830c
brian <bdkuhn602@gmail.com>
2022-04-04 21:17:31 -0700

    reverse checking logic

--- check_server.sh
+++ check_server.sh
@@ -3,8 +3,8 @@
 ping -c1 -W1 -q 10.100.100.2 &>/dev/null
 status=$( echo $? )
 
-if [[ $status == 0 ]]; then
-    echo "host is online"
-else
+if [[ $status != 0 ]]; then
     echo "host is offline"
+else
+    echo "host is online"
 fi
