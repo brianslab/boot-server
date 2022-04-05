63654cc8e3fb131b0f15450a50e985e3f1a76b53
brian <bdkuhn602@gmail.com>
2022-04-04 21:29:28 -0700

    add check for host and vms on server

--- check_server.sh
+++ check_server.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-ping -c1 -W1 -q 10.100.100.2 &>/dev/null
-status=$( echo $? )
+for server in 10.100.100.2 10.100.100.97 10.100.100.52; do
+    ping -c1 -W1 -q $server &>/dev/null
+    status=$( echo $? )
 
-if [[ $status != 0 ]]; then
-    echo "host is offline"
-    ipmitool -H 192.168.5.89 -U ADMIN -P '$PASSWORD' chasis power cycle
-fi
+    if [[ $status != 0 ]]; then
+        echo "$server is offline"
+        ipmitool -H 192.168.5.89 -U ADMIN -P '$PASSWORD' chasis power cycle
+        break
+    fi
+done
